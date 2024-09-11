741a2a139585261c41299f64618357c9535afb32
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-09-10 23:17:31 -0400

    allowing cross origin requests to backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const admin = require("./routes/admin");
 const user = require("./routes/user");
+const cors = require('cors')
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const port = 3000;
 
